eadbed4750c4f7eae47613ed4c8c8cd8f743bbd5
russel <russeljeoff2@gmail.com>
2026-04-08 22:41:45 +0800

    Add initial project files and NuGet configuration for quiz2
    - Created project file `quiz2.csproj` with basic settings for .NET SDK.
    - Added NuGet cache file `project.nuget.cache` to manage package restoration.
    - Included `quiz2.csproj.nuget.dgspec.json` for dependency graph specifications.
    - Generated `quiz2.csproj.nuget.g.props` and `quiz2.csproj.nuget.g.targets` for NuGet integration.

---
+++ midTerm/quiz1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+public static class Program
+{
+    public static double Cube(double a) => ( a * a * a);
+    public static double RectangularPrism(double l, double w, double h) => (l * w * h);
+    public static double Cylinder(double r, double h) => 2 * Math.PI * (r + h);
+    public static double Cone(double r, double l) => Math.PI * (r + l);
+    public static double Sphere(double r) => 4 * Math.PI * (r * r);
+    public static double Hemisphere(double r) => 3 * Math.PI * (r * r);
+
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine(Cube(3));
+        Console.WriteLine(RectangularPrism(10, 20, 25));
+        Console.WriteLine(Cylinder(3, 6));
+        Console.WriteLine(Cone(2, 4));
+        Console.WriteLine(Sphere(3));
+        Console.WriteLine(Hemisphere(2));
+
+        
+        Console.ReadKey();
+
+    }
+}
