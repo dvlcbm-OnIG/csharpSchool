d0c712ff4e346c80818b4f51b5fee6d062777e55
russel <russeljeoff2@gmail.com>
2026-04-14 18:32:41 +0800

    Refactor and update project files
    - Added new index files for content tracking in the .vs directory.
    - Removed obsolete index files from the .vs directory.
    - Updated DocumentLayout and DocumentLayout.backup JSON files to reflect changes in view states.
    - Modified the Program.cs file to enhance functionality with new classes (BankAccount, SavingsAccount, Customer, ElectricBill, Vehicle, FuelVehicle) and improved output formatting for monetary values.
    - Updated binary files (DLLs, EXEs, PDBs) in the Debug directory to reflect the latest build.
    - Adjusted AssemblyInfo files across multiple projects to update informational version attributes.
    - Updated sourcelink JSON files to point to the correct commit hashes.

--- midTerm/mtExam/Program.cs
+++ midTerm/mtExam/Program.cs
@@ -1,4 +1,6 @@
-﻿//1
+﻿//Date finished: 04-14-26
+
+//1
 using System;
 
 public class Person
@@ -22,7 +24,7 @@ public class Employee : Person
         double Salary = hourlyRate * hoursWorked;
 
         DisplayPerson();
-        Console.WriteLine($"Salary: {Salary}");
+        Console.WriteLine($"Salary: {Salary:C}");
     }
 }
 
@@ -35,7 +37,7 @@ public class Product
     public void DisplayProduct()
     {
         Console.WriteLine($"Product Name: {ProductName}");
-        Console.WriteLine($"Price: {Price}");
+        Console.WriteLine($"Price: {Price:C}");
     }
 }
 public class DiscountedProduct : Product
@@ -47,13 +49,91 @@ public class DiscountedProduct : Product
         double FinalPrice = Price - (Price * DiscountedPercent / 100);
 
         DisplayProduct();
-        Console.WriteLine($"Final Price: {FinalPrice}");
+        Console.WriteLine($"Final Price: {FinalPrice:C}");
     }
 }
 
 //3
+public class BankAccount
+{
+    public string AccountName = "Glimpse of You";
+    public double Balance = 041026.00;
+    
+    public void Deposit(double amount)
+    {
+        Console.WriteLine($"Deposit: {amount}");
+    }
+    public void DisplayBalance()
+    {
+        Console.WriteLine($"Account Name: {AccountName}");
+        Console.WriteLine($"Balance: {Balance:C}");
+    }
+}
+public class SavingsAccount : BankAccount
+{
+    public double InterestRate = 0.67;
 
+    public void ComputeInterest()
+    {
+        double Interest = Balance * InterestRate / 100;
+        double NewBalance = Balance + Interest;
 
+        DisplayBalance();
+        Deposit(0);
+        Console.WriteLine($"Interest: {Interest}");
+        Console.WriteLine($"New Balance: {NewBalance:C}");
+    }
+}
+
+//4
+public class Customer
+{
+    public string Name = "Stranger";
+
+    public void DisplayCustomer()
+    {
+        Console.WriteLine($"Name: {Name}");
+    }
+
+}
+public class ElectricBill : Customer
+{
+    public int UnitsConsumed = 10;
+    public double RatePerUnit = 0.54;
+
+    public void ComputedBill()
+    {
+        double Bill = UnitsConsumed * RatePerUnit;
+
+        Console.WriteLine($"Bill: {Bill:C}");
+    }
+}
+
+//5
+public class Vehicle
+{
+    public string VehicleName = "Motorcycle";
+
+    public void DisplayVehicle()
+    {
+        Console.WriteLine($"Vehicle Name: {VehicleName}");
+    }
+
+}
+public class FuelVehicle : Vehicle
+{
+    public double Distance = 20.5;
+    public double FuelEfficiency = 15.59;
+    public double FuelPrice = 135.15;
+
+    public void ComputedFuelCost()
+    {
+        double FuelUsed = Distance / FuelEfficiency;
+        double TotalCost = FuelUsed * FuelPrice;
+
+        Console.WriteLine($"Total Fuel Cost: {TotalCost:C}");
+    }
+}
 
 
 //Main
@@ -63,11 +143,20 @@ public class Program
     {
         Employee emp = new Employee();
         DiscountedProduct dProd = new DiscountedProduct();
+        SavingsAccount sAcc = new SavingsAccount();
+        ElectricBill eBill = new ElectricBill();
+        FuelVehicle fVehicle = new FuelVehicle();
 
+        Console.WriteLine("============ 1 =============");
         emp.ComputeSalary();
+        Console.WriteLine("============ 2 =============");
         dProd.ComputeDiscountedPrice();
-
-
+        Console.WriteLine("============ 3 =============");
+        sAcc.ComputeInterest();
+        Console.WriteLine("============ 4 =============");
+        eBill.ComputedBill();
+        Console.WriteLine("============ 5 =============");
+        fVehicle.ComputedFuelCost();
         Console.ReadKey();
     }
 }
--- midTerm/mtExam/obj/Debug/net10.0/mtExam.AssemblyInfo.cs
+++ midTerm/mtExam/obj/Debug/net10.0/mtExam.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mtExam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67cbd5d376e51f0538b83bfce28fc8aff8f0fddd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6352990daed51b542cc29ebd2a802f13820d9865")]
 [assembly: System.Reflection.AssemblyProductAttribute("mtExam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mtExam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
