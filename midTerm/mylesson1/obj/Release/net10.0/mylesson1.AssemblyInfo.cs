48ad60b5c80012c8a20c94917239a7015215b1d8
russel <russeljeoff2@gmail.com>
2026-05-06 18:38:30 +0800

    Add NuGet support files for folderSeven project
    - Created folderSeven.csproj.nuget.g.props to define NuGet properties.
    - Added folderSeven.csproj.nuget.g.targets for build targets.
    - Generated project.assets.json to manage project dependencies.
    - Included project.nuget.cache for caching NuGet package information.

--- midTerm/mylesson1/obj/Release/net10.0/mylesson1.AssemblyInfo.cs
+++ midTerm/mylesson1/obj/Release/net10.0/mylesson1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mylesson1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab9837c5d13b432d5079616fa0b6c8fd64bebc44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47de8c5ba97910604ea9e121506b9085d5caca64")]
 [assembly: System.Reflection.AssemblyProductAttribute("mylesson1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mylesson1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
