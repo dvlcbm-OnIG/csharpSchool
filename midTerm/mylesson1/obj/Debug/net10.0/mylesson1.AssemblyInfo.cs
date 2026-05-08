0272ec4a0cbb97fb46750f0bae6def5414795650
russel <russeljeoff2@gmail.com>
2026-05-09 01:09:51 +0800

    Update AssemblyInfo and cache files for multiple projects with new informational version

--- midTerm/mylesson1/obj/Debug/net10.0/mylesson1.AssemblyInfo.cs
+++ midTerm/mylesson1/obj/Debug/net10.0/mylesson1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mylesson1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48ad60b5c80012c8a20c94917239a7015215b1d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11b3b1c8566bcfdae1fa3c5d95227e2f56917058")]
 [assembly: System.Reflection.AssemblyProductAttribute("mylesson1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mylesson1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
