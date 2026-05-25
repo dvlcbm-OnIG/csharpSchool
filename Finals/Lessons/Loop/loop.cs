c2410100f865fbdd85fb20751b3a41d04883f6aa
DvlCbm <russeljeoff2@gmail.com>
2026-05-25 08:07:58 +0800

    Update loop.cs

--- Finals/Lessons/Loop/loop.cs
+++ Finals/Lessons/Loop/loop.cs
@@ -1,3 +1,21 @@
 string[] cars = {"Volvo", "BMW", "Ford"};
 // Or more explicitly:
 int[] age = new int[] { 20, 25, 30 }; 
+
+
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+     //1  The countdown
+     for (int i = 5; i>0; i--){
+         Console.WriteLine(i);
+     }
+     Console.WriteLine("Liftoff!");
+     
+     
+    }
+}
