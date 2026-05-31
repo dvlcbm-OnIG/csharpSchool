e50b6b31ba40dab1a39ca424e27f21d4a5f8eeac
DvlCbm <russeljeoff2@gmail.com>
2026-05-31 22:57:14 +0800

    Update Program.cs

--- Finals/Lessons/ForLoops/folderOne/Program.cs
+++ Finals/Lessons/ForLoops/folderOne/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Input a number to be multiplied from 1-100: ");
+        int userIn = Convert.ToInt32(Console.ReadLine());
+        
+        for(int i =1; i<=100; i++){
+            
+            int ans = userIn * i;
+            Console.WriteLine($"{userIn} x {i} = {ans}");
+        };
+    }
+}
