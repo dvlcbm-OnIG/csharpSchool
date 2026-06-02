7325a1594f25fde82ebbde40cb1ebd9ea82ee70b
russel <russeljeoff2@gmail.com>
2026-06-02 16:15:32 +0800

    Refactor Program class and update assembly information for multiple projects

--- Finals/Lessons/ForLoops/folderOne/Program.cs
+++ Finals/Lessons/ForLoops/folderOne/Program.cs
@@ -1,33 +1,4 @@
 ﻿using System;
-<<<<<<< HEAD
-using System.Collections.Generic;
-
-public class Program
-{
-    public static void Main()
-    {
-        string[] names = {"john", "luke", "peter"};
-        
-        List<char> Vowels = new List<char>()
-        {
-            'A', 'E', 'I', 'O', 'U'
-        };
-
-
-        // foreach(string hsa in names)
-        // {
-        //     Console.WriteLine(hsa);
-        // }
-
-        foreach(char letter in Vowels)
-        {
-            Console.Write(letter + " ");
-        }
-
-    }
-}
-=======
-
 public class HelloWorld
 {
     public static void Main(string[] args)
@@ -42,4 +13,3 @@ public class HelloWorld
         };
     }
 }
->>>>>>> e50b6b31ba40dab1a39ca424e27f21d4a5f8eeac
