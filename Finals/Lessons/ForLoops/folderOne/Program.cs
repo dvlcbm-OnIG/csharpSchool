9ca27613daa91faa196051db67fdcdfa08c81b41
russel <russeljeoff2@gmail.com>
2026-06-03 16:07:10 +0800

    Refactor HelloWorld class to demonstrate various for loop implementations and update assembly information for multiple projects

--- Finals/Lessons/ForLoops/folderOne/Program.cs
+++ Finals/Lessons/ForLoops/folderOne/Program.cs
@@ -3,13 +3,73 @@ public class HelloWorld
 {
     public static void Main(string[] args)
     {
-        Console.Write("Input a number to be multiplied from 1-100: ");
-        int userIn = Convert.ToInt32(Console.ReadLine());
-        
-        for(int i =1; i<=100; i++){
-            
-            int ans = userIn * i;
-            Console.WriteLine($"{userIn} x {i} = {ans}");
-        };
+        //1
+        Console.WriteLine("------- 1 -------");
+      for (int i =5; i>=1; i--)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Liftoff!");
+
+
+      //2
+      Console.WriteLine("------- 2 -------");
+      for (int i = 2; i<=10; i+=2)
+        {
+            Console.Write(i + " ");
+        }
+
+       //3
+       Console.WriteLine();
+       Console.WriteLine("------- 3 -------");
+       Console.Write("Enter a word: ");
+       string input = Console.ReadLine();
+
+       for(int i= 1; i<=4; i++)
+        {
+            Console.Write(input + " ");
+        }
+        //4
+        Console.WriteLine();
+        Console.WriteLine("------- 4 -------");
+        for (int i = 3; i<=15; i += 3)
+        {
+            Console.WriteLine(i);
+        }
+
+        //5
+        Console.WriteLine("------- 5 -------");
+        int sum = 0;
+        for (int i = 1; i<=5; i++)
+        {
+            sum +=i;
+        }
+        Console.WriteLine("The total sum is 15");
+
     }
 }
+/*
+Output: 
+
+------- 1 -------
+5
+4
+3
+2
+1
+Liftoff!
+------- 2 -------
+2 4 6 8 10 
+------- 3 -------
+Enter a word: Hello
+Hello Hello Hello Hello 
+------- 4 -------
+3
+6
+9
+12
+15
+------- 5 -------
+The total sum is 15
+
+*/
--- midTerm/mtExam/obj/Debug/net10.0/mtExam.AssemblyInfo.cs
+++ midTerm/mtExam/obj/Debug/net10.0/mtExam.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mtExam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4248afd1bab2d33cc62c036383e9ac4450ef54f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6969c089f6b85d229eae90e64ad9fd271f648b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("mtExam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mtExam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
